3013542e8f8a65e87bf715d19f8275195cff314c
2013_13_mbp <rod@rodkar.com>
2014-09-13 16:59:07 +0800

    Initial Commit

---
+++ BlogReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlogReader
+//
+//  Created by Kar Roderick Sze Hsing on 9/13/14.
+//  Copyright (c) 2014 Cloudyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
